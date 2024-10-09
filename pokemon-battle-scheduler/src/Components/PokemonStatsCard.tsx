378c8827040445cd1127c0fc28fa9fc3d41c4a51
SegFal <92688849+segfal@users.noreply.github.com>
2024-10-09 16:49:50 -0400

    add todo for pokemonstatescard and searchbar

---
+++ pokemon-battle-scheduler/src/Components/PokemonStatsCard.tsx
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { gsap } from 'gsap';
+
+const PokemonStatsCard = () => { // TODO:
+    return null;
+}
+
+
+export default PokemonStatsCard;
---
+++ pokemon-battle-scheduler/src/Components/SearchBar.tsx
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { gsap } from 'gsap';
+
+const SearchBar = () => { // TODO:
+    return null;
+}
+
+
+export default SearchBar;
